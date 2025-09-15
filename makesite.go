3dce2c5fada1854d72b7a882cc01448284784f0d
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2025-09-15 16:17:50 -0700

    Finish v1.0

--- makesite.go
+++ makesite.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"text/template"
+	"os"
+	//"fmt"
+)
 
-func main() {
-	fmt.Println("Hello, world!")
+type Page struct{
+	Content string
+}
+
+func main(){
+
+	//Reads the first post file and stores
+	fileContents, err := os.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err) 
+	}
+
+	// Parse the template file into an object = t
+	t, err :=template.ParseFiles("template.tmpl")
+	if err !=nil{
+		panic(err)
+	}
+
+	// The page now has to get the title and content filled 
+	page := Page{
+		Content: string(fileContents),
+	}
+
+	
+	t.Execute (os.Stdout, page)
+
+	//creates the new file in html 
+	newFile, err := os.Create("first-post.html")
+	if err!=nil{
+		panic(err)
+	}
+
+	t.Execute(newFile,page)
+	newFile.Close()
 }

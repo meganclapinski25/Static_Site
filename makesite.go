48cf6eaa3245da03c07a4d30e1b0fa3ffbe845d3
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2025-09-15 21:13:59 -0700

    finished v1.1

--- makesite.go
+++ makesite.go
@@ -3,7 +3,7 @@ package main
 import (
 	"text/template"
 	"os"
-	//"fmt"
+	"fmt"
 	"flag"
 	"path/filepath"
 	"strings"
@@ -19,8 +19,47 @@ func main(){
 	dir := flag.String("dir", "", " ")
 	flag.Parse()
 
+	// if the --dir is asked, and doesn't equal nothing 
+	
+		added, err := os.ReadDir(*dir)
+		if err != nil {
+			panic(err)
+		}
+		for _, e:= range added{
+			if e.IsDir(){
+				continue
+			}
+			baseFileName := e.Name()
 
-	_ = *dir
+			if !strings.HasSuffix(strings.ToLower(baseFileName), ".txt"){
+				continue
+			}
+
+			fullPath :=filepath.Join(*dir, baseFileName)
+			fmt.Println(fullPath)
+
+			raw, err := os.ReadFile(fullPath)
+			if err != nil {
+				panic(err)
+			}
+
+			page := Page{
+				Content: string(raw),
+			}
+			
+			fmt.Println(page)
+
+			
+
+			outfile := strings.TrimSuffix(fullPath, ".txt") + ".html"
+			f, err := os.Create(outfile)
+			if err != nil{
+				panic(err)
+			}
+			f.Close()
+		}
+		
+	
 	//Reads the first post file and stores
 	fileContents, err := os.ReadFile(*inFile)
 	if err != nil {
